6565c4556bf705bd4a56bd7e5c854d4201e9eb46
Daphne <dg1181@nyu.edu>
2016-01-25 19:06:36 -0800

    Finished clock with happy hour functionality

--- index.js
+++ index.js
@@ -1,1 +1,29 @@
 // Your code here
+var clock = document.getElementById("clock");
+var happyTime = document.getElementById("happyhour");
+var intervalID = window.setInterval(displayClock, 1000);
+
+function ampm(hour) {
+  return (hour <= 12) ? "AM" : "PM";
+}
+
+function happyHour(hour) {
+  return (hour >= 17) ? "IT'S HAPPY HOUR TIME!" : "HAPPY HOUR IS IN ABOUT " + (17 - hour);
+}
+
+function formatTime(unit) {
+  return (unit > 9) ? unit : "0" + unit;
+}
+
+function noMilitary(hours) {
+  return ((hours + 11) % 12) + 1;
+}
+
+function displayClock() {
+  var currentTime = new Date();
+  var hours = currentTime.getHours(), minutes = currentTime.getMinutes(), seconds = currentTime.getSeconds();
+
+
+  clock.innerHTML = noMilitary(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds) + " " + ampm(hours);
+  happyTime.innerHTML = happyHour(hours);
+}
