972cff16c85bac7b0350254f8b574132e126c417
Daphne <dg1181@nyu.edu>
2016-01-26 09:28:23 -0800

    Just realized I could format time in local time instead of military with just one function. Great

--- index.js
+++ index.js
@@ -3,27 +3,29 @@ var clock = document.getElementById("clock");
 var happyTime = document.getElementById("happyhour");
 var intervalID = window.setInterval(displayClock, 1000);
 
-function ampm(hour) {
-  return (hour <= 12) ? "AM" : "PM";
-}
+// function ampm(hour) {
+//   return (hour <= 12) ? "AM" : "PM";
+// }
 
 function happyHour(hour) {
   return (hour >= 17 && hour < 22) ? "IT'S HAPPY HOUR TIME!" : "It's Happy Hour somewhere!";
 }
 
-function formatTime(unit) {
-  return (unit > 9) ? unit : "0" + unit;
-}
+// function formatTime(unit) {
+//   return (unit > 9) ? unit : "0" + unit;
+// }
 
-function noMilitary(hours) {
-  return ((hours + 11) % 12) + 1;
-}
+// function noMilitary(hours) {
+//   return ((hours + 11) % 12) + 1;
+// }
 
 function displayClock() {
   var currentTime = new Date();
-  var hours = currentTime.getHours(), minutes = currentTime.getMinutes(), seconds = currentTime.getSeconds();
+  // var hours = currentTime.getHours(), minutes = currentTime.getMinutes(), seconds = currentTime.getSeconds();
+  var hour = currentTime.getHours();
+  currentTime = currentTime.toLocaleTimeString();
 
-
-  clock.innerHTML = noMilitary(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds) + " " + ampm(hours);
-  happyTime.innerHTML = happyHour(hours);
+  // clock.innerHTML = noMilitary(hours) + ":" + formatTime(minutes) + ":" + formatTime(seconds) + " " + ampm(hours);
+  clock.innerHTML = currentTime;
+  happyTime.innerHTML = happyHour(hour);
 }
